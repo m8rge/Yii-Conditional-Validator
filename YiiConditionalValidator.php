ecb20a906a048ea31679b9566429895407b592b8
Andrey <to.merge@gmail.com>
2014-03-20 11:51:19 +0500

    Update YiiConditionalValidator.php

--- YiiConditionalValidator.php
+++ YiiConditionalValidator.php
@@ -1,256 +1,155 @@
 <?php
+/** @noinspection PhpUndefinedClassInspection */
 
 /**
  * YiiConditionalValidator
  * If-then validation rules on Yii Framework using core validators.
  *
- * Example of rule in a hipothetic invoice checking if id_payment_method is Money
- * or Card, if so, then dueDate is required *and* must be numerical:
- *
- * array('dueDate', 'ext.YiiConditionalValidator',
- *      'if'=>array(
- *          array('id_payment_method', 'in', 'range'=>array(PaymentMethod::MONEY, PaymentMethod::CARD), 'allowEmpty'=>false)
+ * Example:
+ * <code>
+ * // Whole If and Then branches must be present in file only once, so
+ * // Not allowed multiple attributes
+ * // here ↓
+ * array('type', 'ext.validators.YiiConditionalValidator',
+ *      'if' => array(
+ *          array('type', 'compare', 'compareValue' => 1),
  *      ),
- *      'then'=>array(
- *          array('dueDate', 'required', [other params]),
- *          array('dueDate', 'numerical', [other params]),
- *      ),
+ *      'then' => array(
+ *          array('phone', 'match', 'pattern' => '/^7/', 'message' => 'phone must starts with 7'),
+ *          array('name', 'length', 'max' => 255),
+ *      )
  * ),
+ * </code>
  *
  * @author Sidney Lins <solucoes@wmaior.com>
+ * @author Andrey Putilov <to.merge@gmail.com>
  * @copyright Copyright &copy; 2011 Sidney Lins
- * @version 1.0.0
+ * @version 0.9.0
  * @license New BSD Licence
  */
 class YiiConditionalValidator extends CValidator
 {
-
     public $if = array();
     public $then = array();
-    public $ifJS = '';
 
-    private $jsCondition;
-
-
-    public function __construct()
-    {
-        $this->analyseSupliedRules();
-    }
-
+    /**
+     * @param CModel $object
+     * @param string $attribute
+     */
     protected function validateAttribute($object, $attribute)
     {
-        $noErrorsOnIfRules = $this->runValidators($this->prepareValidatorsData($object, $this->if), true, true);
+        $noErrorsOnIfRules = $this->runValidators($object, $this->if, true);
 
         if ($noErrorsOnIfRules) {
-            $this->runValidators($this->prepareValidatorsData($object, $this->then));
-        }
-    }
-
-    /**
-     * Create a data array with params prepared for use into runValidators() method.
-     *
-     * @param CModel $object The original object that is being conditionally validated.
-     * @param array $rules The set of rules defined in "if" or "then" rules.
-     * @return array A dimensional array containing prepared data for each validator
-     * to be created and runned.
-     */
-    protected function prepareValidatorsData($object, array $rules)
-    {
-        $preparedValidatorsData = array();
-
-        foreach ($rules as $ruleKey => $rule) {
-            $validatorData = array();
-
-            /**
-             * @todo Implement way of define "if" rules using or not the attribute
-             * name. When not using, use original attributeName list.
-             */
-            $fetchedObjectAndAttribute   = $this->fetchRealObjectAndAttribute($object, array_shift($rule));
-            $validatorData['object']     = $fetchedObjectAndAttribute['object'];
-            $validatorData['attribute']  = $fetchedObjectAndAttribute['attribute'];
-            $validatorData['validation'] = array_shift($rule);
-
-            foreach ($rule as $key => $ruleParams) {
-                $validatorData['params'][$key] = $ruleParams;
-            }
-
-            $validatorData['params'] = isset($validatorData['params']) ? $validatorData['params'] : array();
-            $preparedValidatorsData[$ruleKey] = $validatorData;
-        }
-
-        return $preparedValidatorsData;
-    }
-
-    /**
-     * Fetches the original or related object defined in a validation rule. If this
-     * attribute name uses dot notation it will try to get the related object, otherwise
-     * the original object is used.
-     *
-     * Ex:
-     *
-     * If original object is an instance of Post and the attribute name is 'author.name',
-     * this will fetch the object $author (related to Post) and its attribute 'name'.
-     *
-     * @param CActiveRecord|CFormModel $originalObject Original object
-     * @param string $attributeName Attribute name defined in validation (may use dot notation)
-     * @return array An array with original or related object and attribute name in format of
-     * ['object' => $instanceOfFetchedObject, 'attribute' => $attributeName].
-     * @throws CException If a relation not exists or if related object is null.
-     */
-    protected function fetchRealObjectAndAttribute($originalObject, $attributeName)
-    {
-        $realObjectAndAttribute = array();
-        $realObjectAndAttribute['object']    = $originalObject;
-        $realObjectAndAttribute['attribute'] = $attributeName;
-
-        if (strpos($attributeName, '.') !== false) {
-            $relations         = explode('.', $attributeName);
-            $realObject        = $originalObject;
-            $realAttributeName = array_pop($relations);
-
-            foreach ($relations as $relation) {
-                if ($realObject instanceof CActiveRecord) {
-                    if ($realObject->hasRelated($relation)) {
-                        $realObject = $realObject->getRelated($relation);
-                    } else {
-                        throw new CException(
-                                get_class($realObject) . " has not a relation named \"$relation\". Check the YiiConditionalValidator rule that is using the attribute name \"$attributeName\"."
-                        );
-                    }
-                }
-            }
-
-            if (is_null($realObject)) {
-                throw new CException(
-                        "The relation \"$relation\" of " . get_class($realObject) . " is returning a null value. Check the YiiConditionalValidator rule that is using the attribute name \"$attributeName\"."
-                );
-            }
-
-            $realObjectAndAttribute['object']    = $realObject;
-            $realObjectAndAttribute['attribute'] = $realAttributeName;
-        }
-
-        return $realObjectAndAttribute;
-    }
-
-    /**
-     * Analyses and validates the rules suplied.
-     *
-     * @throws CException On invalid sintaxe or error.
-     */
-    protected function analyseSupliedRules()
-    {
-        if (!is_array($this->if)) {
-            throw new CException('Invalid argument "validations" for YiiConditionalValidator. Please, suply an array().');
-        }
-
-        if (!is_array($this->then)) {
-            throw new CException('Invalid argument "dependentValidations" for YiiConditionalValidator. Please, suply an array().');
+            $this->runValidators($object, $this->then);
         }
     }
 
     /**
      * Creates and executes each validator based on $validatorsData.
      *
-     * @param array $validatorsData Must contain 'validation', 'object', 'attribute'
-     * and 'params' for each validator.
+     * @param CModel $object
+     * @param array $rules
      * @param boolean $discardErrorsAfterCheck Useful to allow discard validation
      * errors in "if" rules but not in "then" rules.
-     * @param boolean   $returnFalseOnError Useful to allow return false on error
-     * when validating "if" rules but continue checking on error when validating
-     * "then" rules.
-     *
-     * @return boolean If $returnFalseOnError is true, returns false on error, otherwise
-     * returns ever true;
+     * @return boolean error while validating model
      */
-    protected function runValidators(array $validatorsData, $discardErrorsAfterCheck = false, $returnFalseOnError = false)
-    {
-        foreach ($validatorsData as $preparedData) {
-            $validation = $preparedData['validation'];
-            $object     = $preparedData['object'];
-            $attribute  = $preparedData['attribute'];
-            $params     = $preparedData['params'];
+    protected function runValidators($object, $rules, $discardErrorsAfterCheck = false) {
+        $validators = $this->createValidators($object, $rules);
 
-            $errorsBackup = $object->getErrors();
-            $object->clearErrors();
+        $errorsBackup = $object->getErrors();
+        $object->clearErrors();
+        foreach ($validators as $validator) {
+            $validator->validate($object);
 
-            $validator = CValidator::createValidator($validation, $object, $attribute, $params);
-            $validator->validate($object, $attribute);
-
-            if ($object->hasErrors($attribute)) {
-                $newErrors = $object->getErrors();
-                $object->clearErrors();
-
-                if (!$discardErrorsAfterCheck) {
-                    $object->addErrors($newErrors);
+            foreach ($validator->attributes as $attribute) {
+                if ($object->hasErrors($attribute)) {
+                    if ($discardErrorsAfterCheck) {
+                        $object->clearErrors();
+                        $object->addErrors($errorsBackup);
+                        return false;
+                    }
                 }
-
-                if ($returnFalseOnError) {
-                    $object->addErrors($errorsBackup);
-                    $errorsBackup = null;
-                    return false;
-                }
-            } else {
-                $object->addErrors($errorsBackup);
-                $errorsBackup = null;
             }
         }
+
+        $object->addErrors($errorsBackup);
         return true;
     }
 
-
     /**
-        Partial support for clientside conditional validation
-        Due to encapsulation, we can't get JS info regarding other parts of the form - validation is done per field basis.
-        To get this to work, you need to provide javascript condition to perform before field validation.
+     * @param CModel $model
+     * @param array $rules
+     * @return CValidator[]
+     * @throws CException
+     */
+    public function createValidators($model, $rules)
+    {
+        static $existingValidators = array();
 
-        rules = array(
-            array('field1, field2', 'ext.YiiConditionalValidator',
-                'if' => array(
-                    array('field', 'compare', 'compareValue' => 1)
-                ),
-                'ifJS' => "$('#FormID_field').is(':checked')",
-                'then' => array(
-                    array('field1, field2', 'required')
-                )
-        )
+        $cacheKey = md5(serialize($rules) . serialize($model));
+        if (empty($existingValidators[$cacheKey])) {
+            $validators = array();
+            foreach ($rules as $rule) {
+                if (isset($rule[0], $rule[1])) // attributes, validator name
+                {
+                    $validators[] = self::createValidator($rule[1], $model, $rule[0], array_slice($rule, 2));
+                } else {
+                    /** @noinspection PhpUndefinedClassInspection */
+                    throw new CException(
+                        Yii::t(
+                            'yii',
+                            '{class} has an invalid validation rule. The rule must specify attributes to be validated and the validator name.',
+                            array('{class}' => get_class($model))
+                        )
+                    );
+                }
+            }
 
+            $existingValidators[$cacheKey] = $validators;
+        }
 
-
-    */
-  public function clientValidateAttribute($object, $pattribute){
-
-    $validatorsData = $this->prepareValidatorsData($object, $this->then);
-
-    // global JS kill switch for conditional validation
-    $retJS = 'if('. $this->ifJS .') {';
-    $js = '';
-
-    foreach($validatorsData as $preparedData ){
-      $validation = $preparedData['validation'];
-      $object     = $preparedData['object'];
-      $attribute  = $preparedData['attribute'];
-      $params     = $preparedData['params'];
-
-      if( strpos($attribute, $pattribute) !== FALSE){
-
-        $validator = CValidator::createValidator($validation, $object, $pattribute, $params);
-        $js .= $validator->clientValidateAttribute($object, $pattribute);
-
-      }
-
-
+        return $existingValidators[$cacheKey];
     }
 
-    $retJS .= $js;
+    public function clientValidateAttribute($object, $attribute)
+    {
+        $ifValidators = $this->createValidators($object, $this->if);
+        $ifJs = '';
+        $thenValidators = $this->createValidators($object, $this->then);
+        $thenJs = '';
 
-    $retJS .= '}';
+        foreach ($ifValidators as $validator) {
+            foreach ($validator->attributes as $ifAttribute) {
+                $js = $validator->clientValidateAttribute($object, $ifAttribute);
 
-    return $retJS;
+                if (!preg_match('/if\s*?\((.+)\)\s*?\{/s', $js, $matches)) {
+                    throw new CException(
+                        'Error in YiiConditionalValidator: can\'t extract js condition for "if" validator'
+                    );
+                }
+                $if = $matches[1];
+                $if = preg_replace(
+                    '/\bvalue\b/',
+                    'jQuery("#' . CHtml::activeId($object, $ifAttribute) . '").val()',
+                    $if
+                );
+                $ifJs .= $if;
+            }
+        }
 
+        foreach ($thenValidators as $validator) {
+            foreach ($validator->attributes as $thenAttribute) {
+                $then = $validator->clientValidateAttribute($object, $thenAttribute);
+                $then = preg_replace(
+                    '/\bvalue\b/',
+                    'jQuery("#' . CHtml::activeId($object, $thenAttribute) . '").val()',
+                    $then
+                );
+                $thenJs .= $then;
+            }
+        }
 
-  }
-
+        return "\nif($ifJs){{$thenJs}}\n";
+    }
 }
-
-?>
